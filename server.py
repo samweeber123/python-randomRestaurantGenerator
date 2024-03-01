bf361f84836c65fb558881f0a660e6a5ead9b41b
samweeber123 <samwithapple@gmail.com>
2024-03-01 12:12:12 -0500

    Implemented cookies instead of localStorage

--- server.py
+++ server.py
@@ -30,5 +30,5 @@ def get_restaurant():
     )
 
 if __name__ == "__main__":
-    #app.run(debug=True)
-    serve(app, host="0.0.0.0", port=8000)
+    app.run(debug=True)
+    #serve(app, host="0.0.0.0", port=8000)
